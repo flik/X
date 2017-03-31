d9dd6c9ae25b04f30cd3dba747708ca25c14b644
Mohammad Ashfaq <flik2009@gmail.com>
2017-03-31 01:31:30 -0400

    Update X.php
    array to csv and download csv file

--- X.php
+++ X.php
@@ -55,6 +55,40 @@
 				
 	}
 	
+	 // Download csv file
+public static function download_send_headers($filename) {
+    // disable caching
+    $now = gmdate("D, d M Y H:i:s");
+    header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+    header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
+    header("Last-Modified: {$now} GMT");
+
+    // force download  
+    header("Content-Type: application/force-download");
+    header("Content-Type: application/octet-stream");
+    header("Content-Type: application/download");
+
+    // disposition / encoding on response body
+    header("Content-Disposition: attachment;filename={$filename}");
+    header("Content-Transfer-Encoding: binary");
+}
+
+	 //convert array to csv
+public static function array2csv(array &$array)
+{
+   if (count($array) == 0) {
+     return null;
+   }
+   ob_start();
+   $df = fopen("php://output", 'w');
+   fputcsv($df, array_keys(reset($array)));
+   foreach ($array as $row) {
+      fputcsv($df, $row);
+   }
+   fclose($df);
+   return ob_get_clean();
+}
+	 
 	//get array to json
 	public static function getJson($arr=''){
 		 return json_encode($arr);
